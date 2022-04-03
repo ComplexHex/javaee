9db67119f0cec30ba208168981cb0a56b18b8b6a
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-04-03 09:24:24 +0300

    v3

---
+++ JavaEETest/src/main/java/ru/complexhex/JavaEETest/FirstServlet.java
@@ -0,0 +1,25 @@
+package ru.complexhex.JavaEETest;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "FirstServlet", value = "/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter printWriter = response.getWriter();
+
+        printWriter.println("<html>");
+        printWriter.println("<h1> Helloooo wooorld!!! </h1>");
+
+        printWriter.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
