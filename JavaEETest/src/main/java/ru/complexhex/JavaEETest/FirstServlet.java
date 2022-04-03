0708c72dde78bce1dc09ba8bb79706d1f5e006cd
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-04-03 18:39:10 +0300

    v8

--- JavaEETest/src/main/java/ru/complexhex/JavaEETest/FirstServlet.java
+++ JavaEETest/src/main/java/ru/complexhex/JavaEETest/FirstServlet.java
@@ -1,29 +1,48 @@
 package ru.complexhex.JavaEETest;
 
+import ru.complexhex.JavaEETest.cartPackage.Cart;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "FirstServlet", value = "/FirstServlet")
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
 
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+        String user = (String) session.getAttribute("current_user");
+        if (user == null)
+        // response for anonim user
+        // autorization
+        // registration
+        // session.setAttributed("current_user, ID);
 
-        PrintWriter printWriter = response.getWriter();
 
-        printWriter.println("<html>");
-        printWriter.println("<h1> Helloooo" + name + "</h1>");
-        printWriter.println("<h1> Helloooo" + surname + "</h1>");
+//        Cart cart = (Cart) session.getAttribute("cart");
+//
+//        String name = request.getParameter("name");
+//        int quantity = Integer.parseInt(request.getParameter("quantity"));
+//
+//        if (cart == null) {
+//            cart = new Cart();
+//            cart.setName(name);
+//            cart.setQuantity(quantity);
+//        }
+//            session.setAttribute("cart", cart);
 
-        printWriter.println("</html>");
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/testJsp.jsp");
-        dispatcher.forward(request, response);
+//        PrintWriter printWriter = response.getWriter();
+//
+//        printWriter.println("<html>");
+//        printWriter.println("<h1> Count:" + count + "</h1>");
+//
+//        printWriter.println("</html>");
+
+        getServletContext().getRequestDispatcher("/showCart.jsp").forward(request, response);
+
     }
 
     @Override
